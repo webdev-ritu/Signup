fabc2c05736df9a1721d532f3d0918e02dd73776
webdev-ritu <ritu2saxena@gmail.com>
2024-12-02 22:48:55 +0530

    new feature

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ function signUp(username){
 // example
 console.log(signup("newuser")); //output: Sigup successful, please login
 console.log(signup("user")); //output: User Already Registerd, Please login
+
+//feature
+
+function login(username, password) {
+    const registeredUsers = ["user1", "user2", "user3"];
+  
+    if (registeredUsers.includes(username)) {
+      if (password === "Emp@123") {
+        return "Login Successful...";
+      } else {
+        return "Wrong Password...";
+      }
+    } else {
+      return "User Not Found, Please Signup";
+    }
+  }
+  
+  // Example :
+  console.log(login("user1", "Emp@123"));   // Output: Login Successful...
+  console.log(login("user4", "Emp@123"));   // Output: User Not Found, Please Signup
+  console.log(login("user1", "wrongPassword"));  // Output: Wrong Password...
